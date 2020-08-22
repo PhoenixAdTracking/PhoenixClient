3e60e5fba4e9ff86262a5b521cecb08a7f7edfdd
Kiernan Hahn <kiernanhahn@gmail.com>
2020-08-22 14:13:52 -0400

    Added User and Business model POJOs.

---
+++ src/main/java/com/example/phoenix/models/Business.java
@@ -0,0 +1,21 @@
+package com.example.phoenix.models;
+
+import lombok.Data;
+import lombok.Generated;
+
+/**
+ * POJO model class for a business's information.
+ */
+@Data
+@Generated
+public class Business {
+    /**
+     * The id of the business.
+     */
+    private int id;
+
+    /**
+     * The name of the business.
+     */
+    private String name;
+}
---
+++ src/main/java/com/example/phoenix/models/User.java
@@ -0,0 +1,37 @@
+package com.example.phoenix.models;
+
+import lombok.Data;
+import lombok.Generated;
+
+/**
+ * POJO for User info.
+ */
+@Data
+@Generated
+public class User {
+
+    /**
+     * The id of the user.
+     */
+    private int id;
+
+    /**
+     * The first name of the user.
+     */
+    private String firstname;
+
+    /**
+     * The last name of the user.
+     */
+    private String lastname;
+
+    /**
+     * The username the user will use to log in.
+     */
+    private String username;
+
+    /**
+     * The password the user will use to log in.
+     */
+    private String password;
+}
