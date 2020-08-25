ed2a3aae426faa683914723985fc89822d22b0a2
Kiernan Hahn <kiernanhahn@gmail.com>
2020-08-25 15:18:33 -0400

    Hooked PhoenixClientController POST endpoints to Data Processor.

--- src/main/java/com/example/phoenix/PhoenixClientController.java
+++ src/main/java/com/example/phoenix/PhoenixClientController.java
@@ -1,24 +1,40 @@
 package com.example.phoenix;
 
+import com.example.phoenix.ingestion.PhoenixDataProcessor;
 import com.example.phoenix.models.Business;
 import com.example.phoenix.models.User;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.sql.SQLException;
 
 @RestController
 public class PhoenixClientController {
+
+    @Resource(name = "PhoenixDB")
+    private BasicDataSource phoenixDb;
+
+    private PhoenixDataProcessor dataProcessor;
+
+    @PostConstruct
+    public void setup() {
+        this.dataProcessor = new PhoenixDataProcessor(phoenixDb);
+    }
 
     @GetMapping("/ping")
     public String ping() {
         return "pinged!";
     }
 
-    @PostMapping("/business")
-    public int postUser(@RequestBody User user) {
-        return user.getId();
+    @PostMapping("/user")
+    public int postUser(@RequestBody User user) throws SQLException {
+        return dataProcessor.createUser(user);
     }
 
-    @PostMapping("/user")
-    public int postBusiness(@RequestBody Business business) {
-        return business.getId();
+    @PostMapping("/business")
+    public int postBusiness(@RequestBody Business business) throws SQLException {
+        return dataProcessor.createBusiness(business);
     }
 }
