bac9725062667618a6a6110a8482fa5d3369b15a
Kiernan Hahn <kiernanhahn@gmail.com>
2021-01-01 19:02:01 -0500

    Added CORS configuration.

---
+++ src/main/java/com/example/phoenix/authorization/CorsBeanConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.phoenix.authorization;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+/**
+ * Class for configuring Cors Configuration Bean.
+ */
+@Configuration
+public class CorsBeanConfiguration {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("chrome-extension://peejignbmoiaokhlebpmjpfgmnobojdj");
+        corsConfiguration.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        corsConfiguration.setMaxAge(1800L);
+        source.registerCorsConfiguration("/**", corsConfiguration); // you restrict your path here
+        return source;
+    }
+}
